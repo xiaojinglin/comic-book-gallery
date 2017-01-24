50a4f2551a8f844eb77fe911b0d32227be91a83e
xiaojinglin <candylinxiaojing@gmail.com>
2017-01-24 12:09:09 -0500

    Updated comic book reposity

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -55,22 +55,7 @@ namespace ComicBookGallery.Data
                     new Artist() { Name = "Jeff Smith", Role = "Letters" }
                 },
                 Favorite = false
-            },
-             new ComicBook()
-            {
-                Id = 4,
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 500,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
-                }
-            },
+            }
         };
 
         
